9160b0231d87dc3f0a319ffe20388d884e6fbe15
Isaac Ham <apilgrim@uw.edu>
2025-06-11 23:10:28 -0700

    Removed previous JSON files, implemented ones better suited for the project, refactored.

---
+++ Assets/Scripts/Controller/GameManagerMain.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GameManagerMain : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            if (Player.Instance != null)
+            {
+                SaveSystemMain.Save(Player.Instance);
+            }
+            else
+            {
+                Debug.LogWarning("Player instance not found during save.");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F9))
+        {
+            if (Player.Instance != null)
+            {
+                SaveSystemMain.Load(Player.Instance);
+            }
+            else
+            {
+                Debug.LogWarning("Player instance not found during load.");
+            }
+        }
+    }
+}
